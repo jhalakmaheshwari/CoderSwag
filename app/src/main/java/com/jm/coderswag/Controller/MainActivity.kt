580ce5a4dfff1f06bed42feb36fd075390970bb3
jmaheshwari <Jhalak_Maheshwari@intuit.com>
2020-05-18 22:33:15 -0700

    custom adapter view created

--- app/src/main/java/com/jm/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/jm/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jm.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.jm.coderswag.Adapters.CategoryAdapter
 import com.jm.coderswag.Model.Category
 import com.jm.coderswag.R
 import com.jm.coderswag.Services.DataService
@@ -10,14 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adaptor: ArrayAdapter<Category>
+    lateinit var adaptor: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adaptor = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
+        adaptor = CategoryAdapter(this,
             DataService.categories)
         categoryListView.adapter = adaptor
     }
