076758a1ecabf8ad9bd0c2c6e84ca338f5b4f8f4
jmaheshwari <Jhalak_Maheshwari@intuit.com>
2020-05-19 01:45:52 -0700

    product page created with recycler view

--- app/src/main/java/com/jm/coderswag/Services/DataService.kt
+++ app/src/main/java/com/jm/coderswag/Services/DataService.kt
@@ -17,10 +17,18 @@ object DataService {
         Product("Graphic Beanie", "$18", "hat1"),
         Product("Hat Black", "$20", "hat2"),
         Product("Hat White", "$18", "hat3"),
+        Product("Hat Snapback", "$22", "hat4"),
+        Product("Graphic Beanie", "$18", "hat1"),
+        Product("Hat Black", "$20", "hat2"),
+        Product("Hat White", "$18", "hat3"),
         Product("Hat Snapback", "$22", "hat4")
     )
 
     val hoodies = listOf(
+        Product("Hoodie Gray", "$28", "hoodie1"),
+        Product("Hoodie Red", "$20", "hoodie2"),
+        Product("Gray Hoodie", "$28", "hoodie3"),
+        Product("Black Hoodie", "$25", "hoodie4"),
         Product("Hoodie Gray", "$28", "hoodie1"),
         Product("Hoodie Red", "$20", "hoodie2"),
         Product("Gray Hoodie", "$28", "hoodie3"),
@@ -32,6 +40,22 @@ object DataService {
         Product("shirt Badge Light Gray", "$21", "shirt2"),
         Product("shirt Red", "$30", "shirt3"),
         Product("shirt Hustle", "$27", "shirt4"),
+        Product("shirt kickflip studios", "$35", "shirt5"),
+        Product("shirt Black", "$22", "shirt1"),
+        Product("shirt Badge Light Gray", "$21", "shirt2"),
+        Product("shirt Red", "$30", "shirt3"),
+        Product("shirt Hustle", "$27", "shirt4"),
         Product("shirt kickflip studios", "$35", "shirt5")
     )
+
+    val digitalGood = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product> {
+        return when(category) {
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
+        }
+    }
 }
