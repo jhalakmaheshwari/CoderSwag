1b8043b7af2898a41a2159ea1686cc16f419abc4
jmaheshwari <Jhalak_Maheshwari@intuit.com>
2020-05-18 23:26:58 -0700

    Recycler View

--- app/src/main/java/com/jm/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/jm/coderswag/Controller/MainActivity.kt
@@ -3,7 +3,10 @@ package com.jm.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jm.coderswag.Adapters.CategoryAdapter
+import com.jm.coderswag.Adapters.CategoryRecycleAdapter
 import com.jm.coderswag.Model.Category
 import com.jm.coderswag.R
 import com.jm.coderswag.Services.DataService
@@ -11,14 +14,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adaptor: CategoryAdapter
+    lateinit var adaptor: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adaptor = CategoryAdapter(this,
+        adaptor = CategoryRecycleAdapter(this,
             DataService.categories)
         categoryListView.adapter = adaptor
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+
+        // This is for list View
+        /*categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }*/
     }
 }
